d67ca1a5f60cf0cac7fa1724fab30ac600ee0de4
Taemin Lee <taeminlee0601@gmail.com>
2023-02-20 16:52:00 -0500

    Made main.py and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Pong-Game
+This game is made using Python3 and pygame.
+This game is an individual project made for fun.
+This game is just a copy of the game "Pong".
---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+This is an individual project. This game uses Python3 and pygame.
+"""
+
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 900, 500
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong Game")
+
+
+

4cef43ace038a27250bfb4fcefc5945eaece25ae
Taemin Lee <taeminlee0601@gmail.com>
2023-02-20 16:57:17 -0500

    Added assets folder -> Imported font that will be used

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ WIDTH, HEIGHT = 900, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong Game")
 
+FPS = 60
 
+WHITE = 255,255,255
+BLACK = 0,0,0
+
+pygame.time.Clock()
 
